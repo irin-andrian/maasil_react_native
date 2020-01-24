33adeba3c2a5825a5ddd38c423a1d93fe6a5ebfd
Irin Andrian <irintsoa@outlook.fr>
2020-01-24 11:56:54 +0300

    initial commit

---
+++ android/app/src/main/java/com/maasil_react_native_irintsoa/MainActivity.java
@@ -0,0 +1,15 @@
+package com.maasil_react_native_irintsoa;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "maasil_react_native_irintsoa";
+  }
+}
